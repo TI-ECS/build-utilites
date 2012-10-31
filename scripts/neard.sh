580c06f121d3e8edf0c284b7d259bd89704b8f01
Vita Preskovsky <vitap@ti.com>
2012-10-31 15:24:03 +0200

    scripts directory

---
+++ scripts/neard.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+neard=/usr/bin/neard
+
+test -x "$neard" || exit 0
+
+case "$1" in
+  start)
+    echo -n "Starting neard nfc stack"
+    modprobe nfcwilink
+    start-stop-daemon --start --quiet --exec $neard &
+    /usr/share/nfc-demos/enable-adapter nfc0
+    echo "."
+    ;;
+  stop)
+    echo -n "Stopping neard nfc stack"
+    start-stop-daemon --stop --quiet --pidfile /var/run/neard.pid
+    echo "."
+    ;;
+  *)
+    echo "Usage: /etc/init.d/neard.sh {start|stop}"
+    exit 1
+esac
+
+exit 0
+
